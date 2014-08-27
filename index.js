5dd2564abbc4c3edd2306246d78baae38da9dfbf
Alexis Georges <ageorges@atlassian.com>
2014-08-26 21:56:44 -0700

    rename example

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ var React = require('react/addons');
 var Routes = require('react-router/Routes');
 var Route = require('react-router/Route');
 var Prototype = require('./components/Prototype');
-var Example = require('./components/Example');
-var EventStream = require('./components/EventStream');
+var Example1 = require('./components/Example1');
 
 React.renderComponent((
   <Routes>
     <Route handler={Prototype}>
-      <Route name="example" handler={Example} />
-      <Route name="stream" handler={EventStream} />
+      <Route name="example-1" handler={Example1} />
     </Route>
   </Routes>
 ), document.body);
